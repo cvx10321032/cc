3e88a3b41a524969b855a7eb8324b87fa9d7170e
tunche <merj4871@naver.com>
2024-10-25 14:57:24 +0900

    big_num, small_num

---
+++ go/week09/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	answer := rand.Intn(6)
+	fmt.Println(answer)
+
+	var win bool = false
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 :", 3-guesses)
+		in := bufio.NewReader(os.Stdin)
+		i, err := in.ReadString('\n')
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		i = strings.TrimSpace(i)
+		guess, err := strconv.Atoi(i)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(guess)
+
+		if answer == guess {
+			fmt.Println("정답입니다!")
+			win = true
+		} else if answer > guess {
+			fmt.Println("입력하신 수는 정답보다 작은 수 입니다. LOW")
+		} else {
+			fmt.Println("입력하신 수는 정답보다 큰 수 입니다. HIGH")
+		}
+	}
+
+	if win {
+		fmt.Println("이김")
+	} else {
+		fmt.Println("볍신")
+	}
+
+}
