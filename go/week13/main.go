0e9e9fb17c79dd476624a57db25776338be7b190
kim jaemin <cvx3449@gmail.com>
2024-11-22 14:58:20 +0900

    11/22 Slice_3

--- go/week13/main.go
+++ go/week13/main.go
@@ -5,9 +5,14 @@ import (
 )
 
 func main() {
-	var emptySlice []int
-	emptySlice = make([]int, 5)
-	fmt.Printf("%#v\n", emptySlice)
+	var emptySlice []bool
+	emptySlice = make([]bool, 5)
+	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool{nil}, 0 이면 아래에서 1
+
+	if len(emptySlice) == 0 {
+		emptySlice = append(emptySlice, true)
+	}
+	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool{true}, 1
 
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
 	gpa_slice := gpa[1:4] // 4.1, 4.5, 3.9
