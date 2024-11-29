6788464488d14fd47deb7e1c655f59f66008a190
junho <cvx3449@gmail.com>
2024-11-29 14:51:11 +0900

    student

--- go/week14/main.go
+++ go/week14/main.go
@@ -2,23 +2,22 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	ages := make(map[string]int)
+	var student1 student
+	student1.id = 20241234
+	student1.name = "김재민"
+	student1.gpa = 3.5
+	fmt.Println(student1.gpa)
+	var student2 student
+	student2.id = 20244321
+	student2.name = "김민재"
+	student2.gpa = 4.0
+	fmt.Println(student2.id)
 
-	var name string
-	var age int
-
-	for {
-		fmt.Print("What's ur name? (exit to 'q') ")
-		fmt.Scanln(&name)
-		if name == "q" || name == "Q" {
-			break
-		}
-		fmt.Print("Ur age?")
-		fmt.Scanln(&age)
-		ages[name] = age
-	}
-	for name, age := range ages {
-		fmt.Printf("%s is %d year old.\n", name, age)
-	}
 }
